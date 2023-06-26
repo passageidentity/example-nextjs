671643e49b22e3483abff3fa6ae5c7a9180dfd14
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-06-26 10:28:56 +0530

    fixed middleware by authenticating

--- 03-Login-app-directory/middleware.ts
+++ 03-Login-app-directory/middleware.ts
@@ -1,9 +1,19 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import Passage from "@passageidentity/passage-node";
 
 export async function middleware(request: NextRequest) {
   const authToken = request.cookies.get("psg_auth_token")?.value;
   if (!authToken) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  const passage = new Passage({
+    appID: process.env.NEXT_PUBLIC_PASSAGE_APP_ID!,
+  });
+
+  const userID = await passage.validAuthToken(authToken as string);
+  if (!userID) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
